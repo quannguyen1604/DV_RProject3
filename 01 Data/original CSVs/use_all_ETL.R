cd94daf4ac6d7dc598fe857b4fbb7d864b1a3248
Chris Spradling <cjs2599@Chriss-MacBook-Pro.local>
2015-10-11 19:07:51 -0500

    added CSVs and ETLs to 01 Data

---
+++ 01 Data/original CSVs/use_all_ETL.R
@@ -0,0 +1,124 @@
+require(tidyr)
+require(dplyr)
+require(ggplot2)
+
+setwd("~/DataVisualization/DV_RProject3/CSVs/")
+
+#uncomment one at a time
+#file_path <- "use_all_btu.csv"
+file_path <- "use_all_phy.csv"
+
+df <- read.csv(file_path, stringsAsFactors = FALSE)
+
+# Replace "." (i.e., period) with "_" in the column names.
+names(df) <- gsub("\\.+", "_", names(df))
+
+str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
+
+measures <- c("X1960",
+              "X1961",
+              "X1962",
+              "X1963",
+              "X1964",
+              "X1965",
+              "X1966",
+              "X1967",
+              "X1968",
+              "X1969",
+              "X1970",
+              "X1971",
+              "X1972",
+              "X1973",
+              "X1974",
+              "X1975",
+              "X1976",
+              "X1977",
+              "X1978",
+              "X1979",
+              "X1980",
+              "X1981",
+              "X1982",
+              "X1983",
+              "X1984",
+              "X1985",
+              "X1986",
+              "X1987",
+              "X1988",
+              "X1989",
+              "X1990",
+              "X1991",
+              "X1992",
+              "X1993",
+              "X1994",
+              "X1995",
+              "X1996",
+              "X1997",
+              "X1998",
+              "X1999",
+              "X2000",
+              "X2001",
+              "X2002",
+              "X2003",
+              "X2004",
+              "X2005",
+              "X2006",
+              "X2007",
+              "X2008",
+              "X2009",
+              "X2010",
+              "X2011",
+              "X2012",
+              "X2013")
+#measures <- NA # Do this if there are no measures.
+
+# Get rid of special characters in each column.
+# Google ASCII Table to understand the following:
+for(n in names(df)) {
+  df[n] <- data.frame(lapply(df[n], gsub, pattern="[^ -~]",replacement= ""))
+}
+
+dimensions <- setdiff(names(df), measures)
+if( length(measures) > 1 || ! is.na(dimensions)) {
+  for(d in dimensions) {
+    # Get rid of " and ' in dimensions.
+    df[d] <- data.frame(lapply(df[d], gsub, pattern="[\"']",replacement= ""))
+    # Change & to and in dimensions.
+    df[d] <- data.frame(lapply(df[d], gsub, pattern="&",replacement= " and "))
+    # Change : to ; in dimensions.
+    df[d] <- data.frame(lapply(df[d], gsub, pattern=":",replacement= ";"))
+  }
+}
+
+library(lubridate)
+# Fix date columns, this needs to be done by hand because | needs to be correct.
+#                                                        \_/
+df$Order_Date <- gsub(" [0-9]+:.*", "", gsub(" UTC", "", mdy(as.character(df$Order_Date), tz="UTC")))
+df$Ship_Date  <- gsub(" [0-9]+:.*", "", gsub(" UTC", "", mdy(as.character(df$Ship_Date),  tz="UTC")))
+
+# The following is an example of dealing with special cases like making state abbreviations be all upper case.
+# df["State"] <- data.frame(lapply(df["State"], toupper))
+
+# Get rid of all characters in measures except for numbers, the - sign, and period.dimensions
+if( length(measures) > 1 || ! is.na(measures)) {
+  for(m in measures) {
+    df[m] <- data.frame(lapply(df[m], gsub, pattern="[^--.0-9]",replacement= ""))
+  }
+}
+
+write.csv(df, paste(gsub(".csv", "", file_path), ".reformatted.csv", sep=""), row.names=FALSE, na = "")
+
+tableName <- gsub(" +", "_", gsub("[^A-z, 0-9, ]", "", gsub(".csv", "", file_path)))
+sql <- paste("CREATE TABLE", tableName, "(\n-- Change table_name to the table name you want.\n")
+if( length(measures) > 1 || ! is.na(dimensions)) {
+  for(d in dimensions) {
+    sql <- paste(sql, paste(d, "varchar2(4000),\n"))
+  }
+}
+if( length(measures) > 1 || ! is.na(measures)) {
+  for(m in measures) {
+    if(m != tail(measures, n=1)) sql <- paste(sql, paste(m, "number(38,4),\n"))
+    else sql <- paste(sql, paste(m, "number(38,4)\n"))
+  }
+}
+sql <- paste(sql, ");")
+cat(sql)
