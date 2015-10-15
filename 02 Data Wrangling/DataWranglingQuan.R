aadb3023479f89e1bb63664534963eabb609e4a8
Quan Nguyen <quannguyen1604@gmail.com>
2015-10-15 18:07:25 -0500

    update datawranglingquan

---
+++ 02 Data Wrangling/DataWranglingQuan.R
@@ -0,0 +1,12 @@
+require(tidyr)
+require(dplyr)
+require(ggplot2)
+require("jsonlite")
+require("RCurl")
+
+df1 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_prices"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+df2 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_expenditures"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+df <- inner_join(df1,df2, by = "STATE")
+View(df)
