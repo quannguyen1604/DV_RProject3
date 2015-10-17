22ea98b681f7cfdea34635807222c0ab06be141c
unknown <james_h.thorpe@yahoo.com>
2015-10-16 23:11:20 -0500

    Completed my data wrangling/visualization, only need to update the devtracking.

--- 02 Data Wrangling/jamesDataWrangling.R
+++ 02 Data Wrangling/jamesDataWrangling.R
@@ -3,16 +3,6 @@ require(tidyr)
 require(ggplot2)
 require(jsonlite)
 
-energyProductionDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_production_data;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-
-energy_expendituresDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_expenditures;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-
-energy_msn_lookupDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_msn_lookup;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-
-energy_adjusted_pricesDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_adjusted_prices;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-
-energy_usage_btuDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_usage_btu;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-
 energy_pricesDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_prices WHERE x1960 >= -100000000;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
 energy_usage_phyDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_usage_phy WHERE X1960 >= 0;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
@@ -20,8 +10,6 @@ energy_usage_phyDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/
 #Energy production is the odd one out and uses STATECODE as colomn name instead of STATE.Must rename it.
 
 energyProductionDF <- rename(energyProductionDF, STATE = STATECODE)
-
-#need to reformate dataframes into a more usable form using the transposition function t().
 
 # first we Want to filter by state from the consumption dataset, and by MSN code for hydroelectric, nuclear, geothermal, and solar electricity generated, as well as total population. 
 altEnergyDF <- energy_usage_phyDF %>% filter (STATE %in% c("TX", "NV", "CA", "NJ"), MSN %in% c("HYEGP", "NUEGP", "GEEGP", "SOEGP","TPOPP", "WYEGP")) %>% arrange(STATE)
@@ -44,9 +32,9 @@ NV_energy <- df_altEnergy %>% filter (STATE == "NV") %>% select (-DATA_STATUS, -
 NJ_energy <- df_altEnergy %>% filter (STATE == "NJ") %>% select (-DATA_STATUS, -STATE)
 
 
-#transforming and joining the appropriate dataframes into the needed form
+#transforming and joining the appropriate dataframes into the needed form so that we can plot against time
 
-#make cols for price
+#make cols for price using melt
 CA_price <- CA_energy %>% filter (MSN == "ESRCD")
 CA_price <- melt(CA_price, id = "MSN")
 CA_price <- rename(CA_price, ESRCD = value)
@@ -65,7 +53,13 @@ NV_price <- rename(NV_price, ESRCD = value)
 NV_price <- rename(NV_price, YEAR = variable)
 NV_price <- data.frame(NV_price) %>% select(YEAR, ESRCD)
 
-#make cols for geothermal production
+NJ_price <- NJ_energy %>% filter (MSN == "ESRCD")
+NJ_price <- melt(NJ_price, id = "MSN")
+NJ_price <- rename(NJ_price, ESRCD = value)
+NJ_price <- rename(NJ_price, YEAR = variable)
+NJ_price <- data.frame(NJ_price) %>% select(YEAR, ESRCD)
+
+#make cols for geothermal production using melt
 CA_geo <- CA_energy %>% filter (MSN == "GEEGP")
 CA_geo <- melt(CA_geo, id = "MSN")
 CA_geo <- rename(CA_geo, GEEGP = value)
@@ -84,7 +78,13 @@ NV_geo <- rename(NV_geo, GEEGP = value)
 NV_geo <- rename(NV_geo, YEAR = variable)
 NV_geo <- data.frame(NV_geo) %>% select(YEAR, GEEGP)
 
-#make cols for hydroelectricity production
+NJ_geo <- NJ_energy %>% filter (MSN == "GEEGP")
+NJ_geo <- melt(NJ_geo, id = "MSN")
+NJ_geo <- rename(NJ_geo, GEEGP = value)
+NJ_geo <- rename(NJ_geo, YEAR = variable)
+NJ_geo <- data.frame(NJ_geo) %>% select(YEAR, GEEGP)
+
+#make cols for hydroelectricity production using melt
 CA_hy <- CA_energy %>% filter (MSN == "HYEGP")
 CA_hy <- melt(CA_hy, id = "MSN")
 CA_hy <- rename(CA_hy, HYEGP = value)
@@ -103,8 +103,14 @@ NV_hy <- rename(NV_hy, HYEGP = value)
 NV_hy <- rename(NV_hy, YEAR = variable)
 NV_hy <- data.frame(NV_hy) %>% select(YEAR, HYEGP)
 
+NJ_hy <- NJ_energy %>% filter (MSN == "HYEGP")
+NJ_hy <- melt(NJ_hy, id = "MSN")
+NJ_hy <- rename(NJ_hy, HYEGP = value)
+NJ_hy <- rename(NJ_hy, YEAR = variable)
+NJ_hy <- data.frame(NJ_hy) %>% select(YEAR, HYEGP)
 
-# make cols for nuclear electricity production
+
+# make cols for nuclear electricity production using melt
 CA_nu <- CA_energy %>% filter (MSN == "NUEGP")
 CA_nu <- melt(CA_nu, id = "MSN")
 CA_nu <- rename(CA_nu, NUEGP = value)
@@ -123,8 +129,13 @@ NV_nu <- rename(NV_nu, NUEGP = value)
 NV_nu <- rename(NV_nu, YEAR = variable)
 NV_nu <- data.frame(NV_nu) %>% select(YEAR, NUEGP)
 
+NJ_nu <- NJ_energy %>% filter (MSN == "NUEGP")
+NJ_nu <- melt(NJ_nu, id = "MSN")
+NJ_nu <- rename(NJ_nu, NUEGP = value)
+NJ_nu <- rename(NJ_nu, YEAR = variable)
+NJ_nu <- data.frame(NJ_nu) %>% select(YEAR, NUEGP)
 
-# make cols for solar electricity production
+# make cols for solar electricity production using melt
 CA_so <- CA_energy %>% filter (MSN == "SOEGP")
 CA_so <- melt(CA_so, id = "MSN")
 CA_so <- rename(CA_so, SOEGP = value)
@@ -143,7 +154,13 @@ NV_so <- rename(NV_so, SOEGP = value)
 NV_so <- rename(NV_so, YEAR = variable)
 NV_so <- data.frame(NV_so) %>% select(YEAR, SOEGP)
 
-# make cols for wind electricity production
+NJ_so <- NJ_energy %>% filter (MSN == "SOEGP")
+NJ_so <- melt(NJ_so, id = "MSN")
+NJ_so <- rename(NJ_so, SOEGP = value)
+NJ_so <- rename(NJ_so, YEAR = variable)
+NJ_so <- data.frame(NJ_so) %>% select(YEAR, SOEGP)
+
+# make cols for wind electricity production using melt
 CA_wy <- CA_energy %>% filter (MSN == "WYEGP")
 CA_wy <- melt(CA_wy, id = "MSN")
 CA_wy <- rename(CA_wy, WYEGP = value)
@@ -162,7 +179,13 @@ NV_wy <- rename(NV_wy, WYEGP = value)
 NV_wy <- rename(NV_wy, YEAR = variable)
 NV_wy <- data.frame(NV_wy) %>% select(YEAR, WYEGP)
 
-#make cols for total population
+NJ_wy <- NJ_energy %>% filter (MSN == "WYEGP")
+NJ_wy <- melt(NJ_wy, id = "MSN")
+NJ_wy <- rename(NJ_wy, WYEGP = value)
+NJ_wy <- rename(NJ_wy, YEAR = variable)
+NJ_wy <- data.frame(NJ_wy) %>% select(YEAR, WYEGP)
+
+#make cols for total population using melt
 CA_pop <- CA_energy %>% filter (MSN == "TPOPP")
 CA_pop <- melt(CA_pop, id = "MSN")
 CA_pop <- rename(CA_pop, TPOPP = value)
@@ -181,8 +204,14 @@ NV_pop <- rename(NV_pop, TPOPP = value)
 NV_pop <- rename(NV_pop, YEAR = variable)
 NV_pop <- data.frame(NV_pop) %>% select(YEAR, TPOPP)
 
+NJ_pop <- NJ_energy %>% filter (MSN == "TPOPP")
+NJ_pop <- melt(NJ_pop, id = "MSN")
+NJ_pop <- rename(NJ_pop, TPOPP = value)
+NJ_pop <- rename(NJ_pop, YEAR = variable)
+NJ_pop <- data.frame(NJ_pop) %>% select(YEAR, TPOPP)
 
-#join rows in desired form.
+
+#use full_join to aggregate into the full dataset.
 CA_all <- dplyr::full_join(CA_price, CA_geo, "YEAR")
 CA_all <- dplyr::full_join(CA_all, CA_hy, "YEAR")
 CA_all <- dplyr::full_join(CA_all, CA_nu, "YEAR")
@@ -204,32 +233,50 @@ NV_all <- dplyr::full_join(NV_all, NV_so, "YEAR")
 NV_all <- dplyr::full_join(NV_all, NV_wy, "YEAR")
 NV_all <- dplyr::full_join(NV_all, NV_pop, "YEAR")
 
-#mutate in cols that account for population, want produciton per population... Is treating the different values as discrete, not continuous. Need to change.
+NJ_all <- dplyr::full_join(NJ_price, NJ_geo, "YEAR")
+NJ_all <- dplyr::full_join(NJ_all, NJ_hy, "YEAR")
+NJ_all <- dplyr::full_join(NJ_all, NJ_nu, "YEAR")
+NJ_all <- dplyr::full_join(NJ_all, NJ_so, "YEAR")
+NJ_all <- dplyr::full_join(NJ_all, NJ_wy, "YEAR")
+NJ_all <- dplyr::full_join(NJ_all, NJ_pop, "YEAR")
+
+#mutate in cols that account for population, want produciton per population.
 CA_ratio <- CA_all %>% mutate(geo_per_1k =  (as.numeric(GEEGP)/as.numeric(TPOPP)), nuc_per_1k = as.numeric(NUEGP)/as.numeric(TPOPP), hydro_per_1k = as.numeric(HYEGP)/as.numeric(TPOPP), wind_per_1k = as.numeric(WYEGP)/as.numeric(TPOPP), sol_per_1k =as.numeric(SOEGP)/as.numeric(TPOPP), pop = as.numeric (TPOPP), price = as.numeric(ESRCD), year = as.character(YEAR)) %>% select(year, geo_per_1k, nuc_per_1k, hydro_per_1k, wind_per_1k, sol_per_1k)
 
 TX_ratio <- TX_all %>% mutate(geo_per_1k =  (as.numeric(GEEGP)/as.numeric(TPOPP)), nuc_per_1k = as.numeric(NUEGP)/as.numeric(TPOPP), hydro_per_1k = as.numeric(HYEGP)/as.numeric(TPOPP), wind_per_1k = as.numeric(WYEGP)/as.numeric(TPOPP), sol_per_1k =as.numeric(SOEGP)/as.numeric(TPOPP), pop = as.numeric (TPOPP), price = as.numeric(ESRCD), year = as.character(YEAR)) %>% select(year, geo_per_1k, nuc_per_1k, hydro_per_1k, wind_per_1k, sol_per_1k)
 
 NV_ratio <- NV_all %>% mutate(geo_per_1k =  (as.numeric(GEEGP)/as.numeric(TPOPP)), nuc_per_1k = as.numeric(NUEGP)/as.numeric(TPOPP), hydro_per_1k = as.numeric(HYEGP)/as.numeric(TPOPP), wind_per_1k = as.numeric(WYEGP)/as.numeric(TPOPP), sol_per_1k =as.numeric(SOEGP)/as.numeric(TPOPP), pop = as.numeric (TPOPP), price = as.numeric(ESRCD), year = as.character(YEAR)) %>% select(year, geo_per_1k, nuc_per_1k, hydro_per_1k, wind_per_1k, sol_per_1k)
 
+NJ_ratio <- NJ_all %>% mutate(geo_per_1k =  (as.numeric(GEEGP)/as.numeric(TPOPP)), nuc_per_1k = as.numeric(NUEGP)/as.numeric(TPOPP), hydro_per_1k = as.numeric(HYEGP)/as.numeric(TPOPP), wind_per_1k = as.numeric(WYEGP)/as.numeric(TPOPP), sol_per_1k =as.numeric(SOEGP)/as.numeric(TPOPP), pop = as.numeric (TPOPP), price = as.numeric(ESRCD), year = as.character(YEAR)) %>% select(year, geo_per_1k, nuc_per_1k, hydro_per_1k, wind_per_1k, sol_per_1k)
+
+
+# mutate in a STATE col so can differentiate
 CA_ratio <- CA_ratio %>% arrange(year) %>% mutate (STATE = "CA")
 
 TX_ratio <- TX_ratio %>% arrange(year) %>% mutate (STATE = "TX")
 
 NV_ratio <- NV_ratio %>% arrange(year) %>% mutate (STATE = "NV")
 
+NJ_ratio <- NJ_ratio %>% arrange(year) %>% mutate (STATE = "NJ")
+
+#bind rows to create full dataframe to be graphed
 graph_all <- CA_ratio
 graph_all <- dplyr::bind_rows(graph_all, TX_ratio)
 graph_all <- dplyr::bind_rows(graph_all, NV_ratio)
+graph_all <- dplyr::bind_rows(graph_all, NJ_ratio)
+
 
 #once again we melt in order to view multiple variables on one plot
-CA_graph <- melt(CA_ratio, id.vars = "year")
 graph <-  melt(graph_all, id.vars = c("year", "STATE"))
 
 
 ggplot(graph, aes(y = value, x = year, color = variable)) +
+  labs(title='Alternate Energy Production by State') +
+  labs(x="Year", y=paste("Electricity Produced per 1000 citizens")) +
   facet_grid(~STATE)+
   geom_point() +
   geom_line(aes(group = variable)) +
   theme(axis.text.x=element_text(angle=50, size=10, vjust=0.5)) +
+  scale_x_discrete(breaks = c("X1960", "X1965", "X1970", "X1975", "X1980", "X1985", "X1990", "X1995", "X2000", "X2005", "X2010"))
   coord_cartesian()
 
