755a4da1222c67a1a722f6d293528d86b3581b82
unknown <james_h.thorpe@yahoo.com>
2015-10-16 20:23:56 -0500

    Finished data for california

--- 02 Data Wrangling/jamesDataWrangling.R
+++ 02 Data Wrangling/jamesDataWrangling.R
@@ -1,12 +1,13 @@
 require(dplyr)
 require(tidyr)
 require(ggplot2)
+require(jsonlite)
 
 energyProductionDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_production_data;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
 energy_expendituresDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_expenditures;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-energy_pricesDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_prices;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+energy_pricesDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_prices WHERE x1960 >= -100000000;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
 energy_msn_lookupDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_msn_lookup;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
@@ -14,7 +15,7 @@ energy_adjusted_pricesDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84
 
 energy_usage_btuDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_usage_btu;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-energy_usage_phyDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_usage_phy;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+energy_usage_phyDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from energy_usage_phy WHERE X1960 >= 0;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
 #Energy production is the odd one out and uses STATECODE as colomn name instead of STATE.Must rename it.
 
@@ -23,11 +24,17 @@ energyProductionDF <- rename(energyProductionDF, STATE = STATECODE)
 #need to reformate dataframes into a more usable form using the transposition function t().
 
 # first we Want to filter by state from the consumption dataset, and by MSN code for hydroelectric, nuclear, geothermal, and solar electricity generated, as well as total population. 
-altEnergyDF <- energy_usage_phyDF %>% filter (STATE %in% c("TX", "NV", "CA", "NJ"), MSN %in% c("HYEGP", "NUEGP", "GEEGP", "SOEGP","TPOPP", "WYEGP")) %>% arrange(STATE)
+altEnergyDF <- energy_usage_phyDF %>% filter (STATE %in% c("TX", "NV", "CA", "NJ"), MSN %in% c("HYEGP", "NUEGP", "GEEGP", "SOEGP","TPOPP", "WYEGP")) %>% arrange(STATE))
+
+test01 <- energy_usage_phyDF %>% filter (STATE == "CA", MSN == "TPOPP") 
 
 #Filter out the states we're interested in from the price dataset, as well as the (unadjusted) price for electricity in these states, including residential electricity and electricty for all uses (comercial, industrial, etc etc).
 priceDF <- energy_pricesDF %>% filter (STATE %in% c("TX", "NV", "CA", "NJ"), MSN %in% c("ESTCD", "ESRCD"))
 
+
+catest1 <- altEnergyDF %>% filter (STATE == "CA", MSN == "SOEGP")
+catest2 <- priceDF %>% filter (STATE == "CA")
+catest3 <- dplyr::bind_rows(catest2, catest1)
 #Do a row bind (not a col join, our data is weird and the unique variables were placed in the rows, not the colomns) to combine these two data sets.
 df_altEnergy <- dplyr::bind_rows(priceDF, altEnergyDF)
 
@@ -95,7 +102,7 @@ CA_pop <- melt(CA_pop, id = "MSN")
 CA_pop <- rename(CA_pop, TPOPP = value)
 CA_pop <- rename(CA_pop, YEAR = variable)
 CA_pop <- data.frame(CA_pop) %>% select(YEAR, TPOPP)
-View(CA_pop)
+
 
 #join rows in desired form.
 CA_all <- dplyr::full_join(CA_price, CA_geo, "YEAR")
@@ -106,15 +113,13 @@ CA_all <- dplyr::full_join(CA_all, CA_wy, "YEAR")
 CA_all <- dplyr::full_join(CA_all, CA_pop, "YEAR")
 
 #mutate in cols that account for population, want produciton per population... Is treating the different values as discrete, not continuous. Need to change.
-CA_all <- CA_all %>% mutate(geo = as.numeric(GEEGP), nuc = as.numeric(NUEGP), hydro = as.numeric(HYEGP), wind = as.numeric(WYEGP), sol = as.numeric(SOEGP), pop = as.numeric (TPOPP), price = as.numeric(ESRCD), year = as.character(YEAR)) %>% select(year, price, pop, geo, nuc, hydro, wind, sol)
+CA_ratio <- CA_all %>% mutate(geo_per_1k =  (as.numeric(GEEGP)/as.numeric(TPOPP)), nuc_per_1k = as.numeric(NUEGP)/as.numeric(TPOPP), hydro_per_1k = as.numeric(HYEGP)/as.numeric(TPOPP), wind_per_1k = as.numeric(WYEGP)/as.numeric(TPOPP), sol_per_1k =as.numeric(SOEGP)/as.numeric(TPOPP), pop = as.numeric (TPOPP), price = as.numeric(ESRCD), year = as.character(YEAR)) %>% select(year, geo_per_1k, nuc_per_1k, hydro_per_1k, wind_per_1k, sol_per_1k)
 
-CA_all <- CA_all %>% arrange(year)
+CA_ratio <- CA_ratio %>% arrange(year)
 View(CA_all)
 
 #once again we melt in order to view multiple variables on one plot
-CA_graph <- melt(CA_all, id.vars = "year")
-
-View(CA_graph)
+CA_graph <- melt(CA_ratio, id.vars = "year")
 
 ggplot(CA_graph, aes(y = value, x = year, color = variable)) +
   geom_point() +
